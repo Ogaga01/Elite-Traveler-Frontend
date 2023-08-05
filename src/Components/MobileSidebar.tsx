9ea3108ae037ccd5c625b58730c2ee410ae98a2b
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-08-05 10:22:51 +0100

    Finish mobile sidebar

--- src/Components/MobileSidebar.tsx
+++ src/Components/MobileSidebar.tsx
@@ -1,63 +1,54 @@
 import { FC, useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import { NavLink } from "react-router-dom";
-import { AiOutlineCar } from "react-icons/ai";
+import { AiOutlineCar, AiOutlineHome } from "react-icons/ai";
 import { PiAirplaneTakeoff } from "react-icons/pi";
 import { TbReservedLine } from "react-icons/tb";
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from './../Sass/_mobileSidebar.module.scss'
+import styles from "./../Sass/_mobileSidebar.module.scss";
 
 const MobileSidebar: FC = () => {
   const [show, setShow] = useState<boolean>(false);
 
   const handleShow = () => {
     setShow(!show);
-  };    
+  };
 
   return (
     <>
-      <IoMdMenu
-        className={styles['menu']}
-        onClick={handleShow}
-      />
+      {!show && <IoMdMenu className={styles["menu"]} onClick={handleShow} />}
       {show && (
-        <div className={styles['sidebar']}>
+        <div className={styles["sidebar"]}>
           <Header />
-          <nav className={styles['navbar']}>
-            <ul className={styles['navbar_ul']}>
-              <li className={styles['listStyle']} onClick={handleShow}>
-                <NavLink className={styles['']} to="/">
-                  Home
+          <nav className={styles["navbar"]}>
+            <ul className={styles["navbar_ul"]}>
+              <li className={styles["listStyle"]} onClick={handleShow}>
+                <NavLink className={styles["listStyle_link"]} to="/">
+                <span>Home</span>
+                  <span>
+                    <AiOutlineHome />
+                  </span>
                 </NavLink>
               </li>
-              <li className={styles['listStyle']} onClick={handleShow}>
-                <NavLink
-                  className={styles['']}
-                  to="/cars"
-                >
+              <li className={styles["listStyle"]} onClick={handleShow}>
+                <NavLink className={styles["listStyle_link"]} to="/cars">
                   <span>Cars</span>
                   <span>
                     <AiOutlineCar />
                   </span>
                 </NavLink>
               </li>
-              <li className={styles['listStyle']} onClick={handleShow}>
-                <NavLink
-                  className={styles['']}
-                  to="/airplanes"
-                >
+              <li className={styles["listStyle"]} onClick={handleShow}>
+                <NavLink className={styles["listStyle_link"]} to="/airplanes">
                   <span>Airplanes</span>
                   <span>
                     <PiAirplaneTakeoff />
                   </span>
                 </NavLink>
               </li>
-              <li className={styles['listStyle']} onClick={handleShow}>
-                <NavLink
-                  className={styles['']}
-                  to="/reservations"
-                >
+              <li className={styles["listStyle"]} onClick={handleShow}>
+                <NavLink className={styles["listStyle_link"]} to="/reservations">
                   <span>Reservations</span>
                   <span>
                     <TbReservedLine />
